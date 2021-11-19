5ba0c41039596fc48a5361fa4415e4353befa17a
jhordysalinas <jhordy@sumerlabs.com>
2021-11-19 15:12:07 -0500

    cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_qcN7tXkkg', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3jqev4ugoujia8emv587f8h4p3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
